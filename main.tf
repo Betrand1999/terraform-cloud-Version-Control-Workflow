9a0fc5e5be6ee3a4a105280cffcb23ab1cf52559
BETRAND MUTAGHA <betrandmutagha@BETRANDs-MBP.lan>
2024-02-25 16:45:42 -0500

    deploy app

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ instance_type = "t2.micro"
 ami = "ami-0440d3b780d96b29d"
 
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_buckrdfeferrre4et" "example" {
 bucket = "my-tf-test-bucketuyrtrdf"
 
 tags = {
